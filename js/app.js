3ae64e1587730470212f079ccfda01e71d985e08
awmcf <aaronmcfaull@gmail.com>
2019-02-24 18:36:12 +0000

    User can add data which appends to the list on the page

---
+++ js/app.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const newItemform = document.querySelector('#new-item-form');
+  newItemform.addEventListener('submit', handleNewItemFormSubmit);
+
+})
+
+const handleNewItemFormSubmit = function (event) {
+  event.preventDefault();
+
+  const hallOfFameItem = createHallOfFameItem(event.target);
+  const hallOfFame = document.querySelector('#hall-of-fame');
+  hallOfFame.appendChild(hallOfFameItem);
+
+  event.target.reset();
+}
+
+const createHallOfFameItem = function (form) {
+  const hallOfFameItem = document.createElement('li');
+  hallOfFameItem.classList.add('hall-of-fame-item');
+
+  const name = document.createElement('h2');
+  name.textContent = form.name.value;
+  hallOfFameItem.appendChild(name);
+
+  const nationality = document.createElement('h3');
+  nationality.textContent = form.nationality.value;
+  hallOfFameItem.appendChild(nationality);
+
+  const position = document.createElement('p');
+  position.textContent = form.position.value;
+  hallOfFameItem.appendChild(position);
+
+  return hallOfFameItem;
+}
